01e52e02d6f077b4d3638416fbb2a952130503c5
martin <91495180+fih29b@users.noreply.github.com>
2022-07-18 18:37:39 +0200

    ELTE university user

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,5 @@
-import React, {useEffect, useState} from 'react';
 import { render, screen } from '@testing-library/react';
 import Home from './pages/Home';
-import List from './components/List';
-import { getCats } from './services/getCats';
-import App from './App';
-import {Cat} from "./types/cats";
 
 test('renders learn react link', () => {
   /*render(<App />);
