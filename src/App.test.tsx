9479a4789b1f9fd50557027e09f5173c0bb2b77d
yerle89 <martinyerle@gmail.com>
2022-07-18 18:46:40 +0200

    last commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import Home from './pages/Home';
+import App from "./App";
 
 test('renders learn react link', () => {
   /*render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Random List of Cats Facts/i);
   expect(linkElement).toBeInTheDocument();*/
-  //const aux = getCats();
 });
 
 test('new facts buttons loaded', () => {
